4c1315816849fa156ef1c893d7da0310784f43a2
hamed.farag <hamed.farag.2009@gmail.com>
2018-03-03 15:35:39 -0800

    Add Code Component

---
+++ src/documentation/_components/Code/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from './styles.scss';
+
+export default function ({ code }) {
+    return (
+        <div className="Hub-documentation-code">
+            {code}
+        </div>
+    )
+}
