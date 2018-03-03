6272a1524af489498df2efe3dfb046fdd6c004a9
hamed.farag <hamed.farag.2009@gmail.com>
2018-03-03 15:35:28 -0800

    Enhance Section Component

--- src/documentation/_components/Section/index.js
+++ src/documentation/_components/Section/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 import styles from './styles.scss';
+import Code from '../Code';
 
 export default function ({ title, body, subBody, children, isBodyHTML }) {
     return (
         <div className="Hub-documentation-section">
             <h1>{title}</h1>
-            {isBodyHTML ? <div dangerouslySetInnerHTML={{ __html: body }}></div> : <div>{body}</div>}
-            {isBodyHTML ? <div dangerouslySetInnerHTML={{ __html: subBody }}></div> : <div>{subBody}</div>}
-            <div>
+            {isBodyHTML ? <div className="Hub-documentation-section__Content" dangerouslySetInnerHTML={{ __html: body }}></div> : <div className="Hub-documentation-section__Content">{body}</div>}
+            {isBodyHTML ? <div className="Hub-documentation-section__Content" dangerouslySetInnerHTML={{ __html: subBody }}></div> : <div className="Hub-documentation-section__Content">{subBody}</div>}
+            <div className="Hub-documentation-section__Content">
                 {children}
             </div>
         </div>
