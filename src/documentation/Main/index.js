4084a37a3ccf13c50d07285b553a9751c489d91a
hamed.farag <hamed.farag.2009@gmail.com>
2018-03-03 20:12:22 -0800

    Add Git URL

--- src/documentation/Main/index.js
+++ src/documentation/Main/index.js
@@ -28,6 +28,7 @@ class Main extends React.Component {
 			'/': 'Introduction',
 			'/gettingstart': 'Getting Started'
 		}
+		this.gitUrl = 'https://github.com/hamed-farag/FM';
 	}
 
 	closeSideBar() {
@@ -47,7 +48,7 @@ class Main extends React.Component {
 							<div></div>
 						</div>
 						<h2 className="hub-documentation-header-component">{this.componentName[this.props.location.pathname]}</h2>
-						<div className="hub-documentation-header-logo">
+						<div className="hub-documentation-header-logo" onClick={() => {window.open(this.gitUrl)}}>
 							<svg className="hub-documentation-header-logo-svg" focusable="false" viewBox="0 0 24 24"><path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"></path></svg>
 						</div>
 					</div>
