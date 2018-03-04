faa4195237ba358f2f12d47adbaabd59ccfdec7f
hamed.farag <hamed.farag.2009@gmail.com>
2018-03-03 16:09:18 -0800

    Enhance Code Component

--- src/documentation/_components/Code/index.js
+++ src/documentation/_components/Code/index.js
@@ -2,10 +2,14 @@ import React from "react";
 
 import styles from './styles.scss';
 
-export default function ({ code }) {
+export default function ({ title, description, code }) {
     return (
         <div className="Hub-documentation-code">
-            {code}
+            <h4 className="Hub-documentation-code__title">{title}</h4>
+            <p>{description}</p>
+            <div className="Hub-documentation-code__body">
+                {code}
+            </div>
         </div>
     )
 }
