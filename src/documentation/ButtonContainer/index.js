0f52be2c7fc0af530d60d4c09e0e9d60e36e2ce4
hamed.farag <hamed.farag.2009@gmail.com>
2018-03-03 19:32:14 -0800

    Enhance Buton Container Documentation

--- src/documentation/ButtonContainer/index.js
+++ src/documentation/ButtonContainer/index.js
@@ -17,57 +17,18 @@ export default function ({ }) {
 	return (
 		<div className="ButtonContainer">
 			<Section title={_button.title} body={_button.main} />
-			<Preview title="Examples" desciption="Bootstrap includes several predefined button styles, each serving its own semantic purpose, with a few extras thrown in for more control." >
+			<Preview title="Examples" description="Nebula-UI includes several predefined button styles, each serving its own semantic purpose, with a few extras thrown in for more control." >
 				<div className="ButtonContainer__Button"><Button title="Primary" /></div>
 				<div className="ButtonContainer__Button"><Button type="hub-button-secondary" title="Secondary" /></div>
 				<div className="ButtonContainer__Button"><Button type="hub-button-primary hub-button-large" title="Large" /></div>
 				<div className="ButtonContainer__Button"><Button buttonState="disabled" title="Disabled" /></div>
 			</Preview>
 			<Code title="Code">
-				<div>{`<button type="button" class="hub-button hub-button-primary"><span>Primary</span></button>`}</div>
-				<div>{`<button type="button" class="hub-button hub-button-secondary"><span>Secondary</span></button>`}</div>
-				<div>{`<button type="button" class="hub-button hub-button-primary hub-button-large"><span>Large</span></button>`}</div>
-				<div>{`<button type="button" class="hub-button hub-button-primary" disabled=""><span>Disabled</span></button>`}</div>
+				<div className="ButtonContainer__Button__Code">{`<button type="button" class="hub-button hub-button-primary"><span>Primary</span></button>`}</div>
+				<div className="ButtonContainer__Button__Code">{`<button type="button" class="hub-button hub-button-secondary"><span>Secondary</span></button>`}</div>
+				<div className="ButtonContainer__Button__Code">{`<button type="button" class="hub-button hub-button-primary hub-button-large"><span>Large</span></button>`}</div>
+				<div className="ButtonContainer__Button__Code">{`<button type="button" class="hub-button hub-button-primary" disabled=""><span>Disabled</span></button>`}</div>
 			</Code>
 		</div>
 	)
 }
-
-
-
-/*
-const ButtonContainer = () => {
-  return (
-    <div className="hub-grid">
-      <div className="hub-row">
-        <div className="hub-column hub-width-sm-12 hub-width-md-3">
-          <Button />
-        </div>
-        <div className="hub-column hub-width-sm-12 hub-width-md-6">
-          <Button type="hub-button-primary hub-button-large" />
-        </div>
-      </div>
-      <div className="hub-row">this is a primary Button</div>
-      <div className="hub-row">
-        <div className="hub-column hub-width-sm-12 hub-width-md-3">
-          <Button type="hub-button-secondary" />
-        </div>
-        <div className="hub-column hub-width-sm-12 hub-width-md-6">
-          <Button type="hub-button-secondary hub-button-large" />
-        </div>
-      </div>
-      <div className="hub-row">this is a secondary Button</div>
-      <div className="hub-row">
-        <div className="hub-column hub-width-sm-12 hub-width-md-3">
-          <Button buttonState="disabled" />
-        </div>
-        <div className="hub-column hub-width-sm-12 hub-width-md-6">
-          <Button type="hub-button-large" buttonState="disabled" />
-        </div>
-      </div>
-      <div className="hub-row">this is a disabled Button</div>
-    </div>
-  );
-};
-export default ButtonContainer;
-*/
