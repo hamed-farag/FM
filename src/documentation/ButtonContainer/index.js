585c4144170d78f12b176f5d46d5f974eaae89d0
hamed.farag <hamed.farag.2009@gmail.com>
2018-03-03 17:56:48 -0800

    Enhance Button Documentation

--- src/documentation/ButtonContainer/index.js
+++ src/documentation/ButtonContainer/index.js
@@ -1,8 +1,38 @@
 import React from "react";
+import styles from './styles.scss';
 
 //Components
 import Button from "../../components/Button";
 
+import Section from '../_components/Section';
+import Code from '../_components/Code';
+import Preview from '../_components/Preview';
+
+const _button = {
+	title: 'Button',
+	main: `Use HUB button styles for actions in forms, dialogs, and more with support for multiple sizes, states, and more.`
+}
+
+export default function ({ }) {
+	return (
+		<div className="ButtonContainer">
+			<Section title={_button.title} body={_button.main} />
+			<Preview title="Examples" desciption="Bootstrap includes several predefined button styles, each serving its own semantic purpose, with a few extras thrown in for more control." >
+				<div className="ButtonContainer__Button"><Button title="Primary" /></div>
+				<div className="ButtonContainer__Button"><Button type="hub-button-secondary" title="Secondary" /></div>
+				<div className="ButtonContainer__Button"><Button type="hub-button-primary hub-button-large" title="Large" /></div>
+				<div className="ButtonContainer__Button"><Button buttonState="disabled" title="Disabled" /></div>
+			</Preview>
+			<Code title="Code">
+
+			</Code>
+		</div>
+	)
+}
+
+
+
+/*
 const ButtonContainer = () => {
   return (
     <div className="hub-grid">
@@ -37,3 +67,4 @@ const ButtonContainer = () => {
   );
 };
 export default ButtonContainer;
+*/
