87ff82fa1ea70c4e787dec667f6b33cbc769672d
hamed.farag <hamed.farag.2009@gmail.com>
2018-03-03 16:09:39 -0800

    Enhance Contents

--- src/documentation/_content/index.js
+++ src/documentation/_content/index.js
@@ -2,6 +2,6 @@ export default {
     philosophy: {
         title: 'Philosophy',
         main: `A nebula is an interstellar cloud of dust, hydrogen, helium and other ionized gases. Nebulae are often star-forming regions In these regions the formations of gas, dust, and other materials "clump" together to form denser regions, which attract further matter, and eventually will become dense enough to form stars.`,
-        sub: ` Nebula framework contains UI and JS Components so you you can build with them  your design and focus on your business and to make your app look and feel the ITWORX HUB Product.`
+        sub: `Nebula framework contains UI and JS Components so you can build your design and focus on your business and make your app looks like ITWORX HUB Design.`
     }
 }
